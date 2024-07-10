ae7c59bc03aa530b12d76d72baf05c6022b520d7
Bianca Sanches <biancasanches.dev@gmail.com>
2024-07-10 20:32:56 -0300

    start-project

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+    return(
+        <View className='flex-1 justify-center items-center'>
+            <Text className='text-white'>Hello world!</Text>
+        </View>
+    )
+};
